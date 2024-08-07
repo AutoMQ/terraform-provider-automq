ca21b86061977ae56db376400f4a4117a04f3eec
kris20030907 <3185633428@qq.com>
2024-08-07 14:37:22 +0800

    feat: deploy CMP control center on AWS using terraform

---
+++ modules/aws-cn-module/main.tf
@@ -0,0 +1,66 @@
+# main.tf
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+data "aws_vpc" "selected" {
+  id = var.aws_vpc_id
+}
+
+data "aws_subnets" "all" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+}
+
+data "aws_subnet" "first" {
+  id = data.aws_subnets.all.ids[0]
+}
+
+resource "aws_security_group" "allow_all" {
+  vpc_id = data.aws_vpc.selected.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                   = var.aws_ami_id
+  instance_type         = "c5.xlarge"
+  subnet_id             = data.aws_subnet.first.id
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp2"
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sdh"
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "cmp-service"
+  }
+}
+
+resource "aws_eip" "web_ip" {
+  instance = aws_instance.web.id
+}
