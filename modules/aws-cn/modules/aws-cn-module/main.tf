8bcb363aed61459ee20638a08008d74800160d42
kris20030907 <3185633428@qq.com>
2024-08-08 11:53:12 +0800

    feat: add ports

--- modules/aws-cn/modules/aws-cn-module/main.tf
+++ modules/aws-cn/modules/aws-cn-module/main.tf
@@ -31,6 +31,20 @@ resource "aws_security_group" "allow_all" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
